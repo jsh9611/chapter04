dca5430d88e7434eb80877d6d7e85b95d5f82a8b
JangSH <JangSH@JSHComputer>
2019-11-26 20:05:49 +0900

    util.Calendar

--- src/chapter04/CalendarTest.java
+++ src/chapter04/CalendarTest.java
@@ -7,42 +7,50 @@ public class CalendarTest {
 	public static void main(String[] args) {
 		Calendar cal = Calendar.getInstance();
 		printDate(cal);
-
+		
+		cal.set(Calendar.YEAR, 2020);
+		cal.set(Calendar.MONTH, 10); //MONTH(11) - 1;
+		cal.set(Calendar.DATE, 26);
+		printDate(cal);
+		
+		cal.set(2016, 02, 29);
+		cal.add(Calendar.DATE, 365);
+		printDate(cal);
+		
 	}
 	
 	public static void printDate(Calendar cal) {
-		String[] days = {"일", "월", "화", "수", "목", "금", "토"};
+		String[] days = { "일", "월", "화", "수", "목", "금", "토", "일" };  
 		
-		//년도
+		// 년도
 		int year = cal.get(Calendar.YEAR);
 		
-		// 월
+		// 월(0~11, +1)
 		int month = cal.get(Calendar.MONTH);
-		
+
 		// 일
-		int day = cal.get(Calendar.DATE);
+		int date = cal.get(Calendar.DATE);
 		
 		// 요일[1(일)~7(토)]
-		int date = cal.get(Calendar.DAY_OF_WEEK);
+		int day = cal.get(Calendar.DAY_OF_WEEK);
 		
 		// 시
 		int hour = cal.get(Calendar.HOUR);
 		
 		// 분
 		int minute = cal.get(Calendar.MINUTE);
-		
+
 		// 초
 		int second = cal.get(Calendar.SECOND);
 		
 		System.out.println(
-				year	+ "-" +
-				(month+1)	+"-" +				
-				day	+ "-" +
-				date + "-" +
-				hour	+ "-" +
-				minute	+ "-" +
-				second	+ "-"
-				);
+				year        + "-" +
+			    (month+1)   + "-" +
+				date        + "(" + 
+				days[day-1] + ") " +
+			    hour        + ":" + 
+				minute      + ":" +
+			    second);
 	}
 
 }
